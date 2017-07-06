91b901118f434309fe34510cacbc89781e79c2d9
przemwo <przemwo@o2.pl>
2017-07-06 18:22:03 +0200

    functions

--- app/app.js
+++ app/app.js
@@ -1,4 +1,18 @@
 function startGame() {
-    console.log('Hello world!!!');
+    var name = getInputValue('name');
+    postName(name);
 }
-startGame();
+function postName(name) {
+    var nameElement = document.getElementById('postedName');
+    nameElement.innerText = "My name is: " + name;
+}
+function getInputValue(elementID) {
+    var inputElement = document.getElementById(elementID);
+    if (inputElement.value === '') {
+        return undefined;
+    }
+    else {
+        return inputElement.value;
+    }
+}
+document.getElementById('start').addEventListener('click', startGame);
--- app/app.ts
+++ app/app.ts
@@ -1,4 +1,17 @@
-function startGame() {
-    console.log('Hello world!!!');
+function startGame():void {
+  let name: string | undefined = getInputValue('name');
+  postName(name);
 }
-startGame();
+function postName(name?: string): void {
+  let nameElement: HTMLElement | null = document.getElementById('postedName');
+  nameElement!.innerText = `My name is: ${name}`;
+}
+function getInputValue(elementID: string): string | undefined {
+  let inputElement: HTMLInputElement = <HTMLInputElement>document.getElementById(elementID);
+  if (inputElement.value === '') {
+    return undefined;
+  } else {
+    return inputElement.value;
+  }
+}
+document.getElementById('start')!.addEventListener('click', startGame);
